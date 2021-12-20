030076a3ee2bdfd72f352410fb6ac3aa64d5e162
KryptoOnn <83477843+KryptoOnn@users.noreply.github.com>
2021-12-20 12:40:06 -0800

    Add files via upload

---
+++ Krypted Loader [SIMPLE]/Krypted Loader [SIMPLE]/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+A Simple Cheat Loader I Created Design Isn't Too Crazy But Simple And Nice
+Loader Created By KryptoOnFunDip#2800 [OPEN SOURCE]
+You May Also Sell, Modify And Release This As Your Own. As I Really Don't Care What You Do With It
+Contact Me For Any Help! 
+DISCORD : KryptoOnFunDip#2800
+
+[NOTES]
+This Form Is A Blank Form I Will Let You Design
+You Can Add Or Code Whatever To This Form
+I Will Not Be Updating This Project As This Was Only Created As A Base
+Anyways Have Fun Coding!
+[NOTES]
+*/
+
+namespace Krypted_Loader__SIMPLE_
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
